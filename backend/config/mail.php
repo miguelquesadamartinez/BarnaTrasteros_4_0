c4fada207209eba6308fc8e61ad0ac6c538abe14
miguelquesadamartinez <miguel.quesada.martinez.1975@gmail.com>
2026-03-18 13:12:43 +0100

    Update mail.php

--- backend/config/mail.php
+++ backend/config/mail.php
@@ -111,8 +111,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'miguel.quesada.martinez.1975@gmail.com'),
+        'name' => env('MAIL_FROM_NAME', 'TeTe'),
     ],
 
     'reportes' => [
